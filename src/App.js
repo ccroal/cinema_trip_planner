0168c6dd71fde47a824c432faa4d3598bb5326e8
Ally Conway <avconway@hotmail.co.uk>
2019-03-02 14:07:33 +0000

    Adds postcode search function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Search from './components/main/Search.js';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      postcode: ''
+    };
+    this.handlePostcodeInput = this.handlePostcodeInput.bind(this);
+  }
+
+  handlePostcodeInput(postcode) {
+    console.log('Entered postcode:', postcode);
+  }
+
   render() {
     return (
-
-      <h1>This is our app</h1>
+      <div>
+        <h1>This is our app</h1>
+        <Search onPostcodeSubmit={this.handlePostcodeInput}/>
+      </div>
     );
   }
+
+
 }
 
 export default App;
---
+++ src/components/main/Search.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      postcode: ''
+    };
+    this.handlePostcodeChange = this.handlePostcodeChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const postcode = this.state.postcode.trim();
+    if (!postcode) {
+      return
+    }
+    const regex = / /g;
+    const amendedPostcode = postcode.replace(regex, '');
+    this.props.onPostcodeSubmit({ postcode: amendedPostcode });
+    this.setState({ postcode: '' });
+  }
+
+  handlePostcodeChange(event) {
+    this.setState({ postcode: event.target.value });
+  }
+
+  render() {
+    return (
+      <form className="postcode-search" onSubmit={this.handleSubmit}>
+
+      <label>
+      Please enter your postcode:
+      <input type="text" placeholder="Your postcode here..." value={this.state.postcode} onChange={this.handlePostcodeChange}/>
+      </label>
+
+      <input type="submit" value="View cinemas and films in your area" />
+      </form>
+    )
+  }
+}
+
+export default Search;
