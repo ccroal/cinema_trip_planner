870633515e81e0f5abeece7a05463a69874cbe3c
Ally Conway <avconway@hotmail.co.uk>
2019-03-04 10:46:45 +0000

    Adds buttons for screening times

--- src/components/cinema_search/FilmScreenings.js
+++ src/components/cinema_search/FilmScreenings.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 const FilmScreenings = (props) => {
+
+
+  const onSelection = (event) => {
+    console.log(event.target.value);
+  }
+
   if(!props.title || !props.times){
     return (
       <p>waiting to load</p>
     )
   }
+  const allTimings = props.times.map((time) => {
+    return <button onClick={onSelection} value={time}>{time}</button>
+  })
 
   return (
     <div>
     <p>{props.title}</p>
-    <p>{props.times}</p>
+    {allTimings}
     </div>
   )
 }
 export default FilmScreenings;
+
+
+//for each time,
+// create an a tag
+// add a space
--- src/containers/CinemaSearchResult.js
+++ src/containers/CinemaSearchResult.js
@@ -4,23 +4,29 @@ import FilmScreenings from '../components/cinema_search/FilmScreenings.js';
 const CinemaSearchResult = (props) => {
 
   if(!props.cinemaScreenings){
-    return <p>Waiting to load</p>
+    return null;
   }
 
   const screenings = props.cinemaScreenings.map((film) => {
     return (
 
       <li className="screening-list-item">
-      <FilmScreenings title={film.title} times={film.times}/>
+      <FilmScreenings title={film.title} times={film.times} />
       </li>
   )
   })
+// handleScreeningSelection={handleScreeningSelection}
+// function handleScreeningSelection(time) {
+//   console.log(time);
+// }
 
 return (
-
+  <div>
+  <p>Please select a screening time below:</p>
   <ul className="screenings-list">
     {screenings}
     </ul>
+    </div>
 )
 
 }
