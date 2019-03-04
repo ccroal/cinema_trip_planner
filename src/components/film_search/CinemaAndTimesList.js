4f898490d942b418283cb7112ac5fd90f252235e
Christopher Croal <ccroal@ntlworld.com>
2019-03-04 19:56:09 +0000

    select time button passes data back to main

--- src/components/film_search/CinemaAndTimesList.js
+++ src/components/film_search/CinemaAndTimesList.js
@@ -3,15 +3,20 @@ import CinemaAndTimes from './CinemaAndTimes';
 
 const CinemaAndTimesList = (props) => {
 
+
+
   const allCinemas = props.allCinemaData
-  console.log('allCinemas', allCinemas);
+
   const cinemasAndTimesList = props.allListingsData.map((cinema) => {
     return (<li className="cinema-item">
-      <CinemaAndTimes cinemaAndTimes={cinema} allCinemaData={allCinemas} />
+      <CinemaAndTimes cinemaAndTimes={cinema} allCinemaData={allCinemas} timeSelection={handleTimeSelection}/>
       </li>)
   })
 
- console.log('cinemaAndTimes list', cinemasAndTimesList);
+  function handleTimeSelection(time, filmTitle, cinema_id){
+    props.timeSelection(time, filmTitle, cinema_id);
+  }
+
 
   return(
     <ul className="cinema-and-times-list-component" >
