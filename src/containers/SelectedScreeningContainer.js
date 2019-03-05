9dd3e4fadd4abbdde21430bf0128f828a9c090ca
Christopher Croal <ccroal@ntlworld.com>
2019-03-05 16:17:50 +0000

    added random quote generator

--- src/containers/SelectedScreeningContainer.js
+++ src/containers/SelectedScreeningContainer.js
@@ -10,10 +10,28 @@ class SelectedScreeningContainer extends Component {
     super(props);
   }
 
+  randomQuotes(){
+    const quotes = [
+     " “I'm going to make him an offer he can't refuse.” The Godfather, 1972 ",
+     "“Toto, I've got a feeling we're not in Kansas anymore.” The Wizard of Oz, 1939",
+     "“Here's looking at you, kid.” Casablanca, 1942",
+     "“Fasten your seatbelts. It's going to be a bumpy night.” All About Eve, 1950",
+     "“What we've got here is failure to communicate.” Cool Hand Luke, 1967",
+     "“E.T. phone home.” E.T. The Extra-Terrestrial, 1982",
+     "“They call me Mister Tibbs!” In the Heat of the Night, 1967",
+     "“Made it, Ma! Top of the world!” White Heat, 1949",
+     "I'm as mad as hell, and I'm not going to take this anymore!” Network, 1976",
+     "“Gentlemen, you can't fight in here! This is the War Room! Dr. Strangelove 1964"
+    ]
+    const randomquote = quotes[Math.floor(Math.random()*quotes.length)]
+    return randomquote
+  }
+
   render(){
+
     if(!this.props.routeObject){
-      return null
-    }
+        return this.randomQuotes()
+          }
 
     return (
       <div>
@@ -21,6 +39,7 @@ class SelectedScreeningContainer extends Component {
         <TransportComponent routeObject={this.props.routeObject} />
       </div>
     )
+
   }
 }
 
